23d86f4ec218b0be9c866f288c605014feaea02e
Yasin Arafat <mdyasinarafat3@gmail.com>
2026-08-18 14:11:36 +0200

    Point links at the renamed Dynamic-Braking-System repo
    The repository was renamed to fix the brake/break spelling. GitHub
    redirects the old name, but update the links so nothing depends on it.

--- Carla/Distance_estimate_mobilenetssd_carla.py
+++ Carla/Distance_estimate_mobilenetssd_carla.py
@@ -6,8 +6,8 @@ from __future__ import print_function
 import urllib.request
 
 # URLs for the model files (note the ?raw=true to get the raw content)
-url_pb = "https://github.com/ArafatTheGoldenBoy/Dynamic-Breaking-System/blob/main/Carla/frozen_inference_graph.pb?raw=true"
-url_pbtxt = "https://github.com/ArafatTheGoldenBoy/Dynamic-Breaking-System/blob/main/Carla/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt?raw=true"
+url_pb = "https://github.com/ArafatTheGoldenBoy/Dynamic-Braking-System/blob/main/Carla/frozen_inference_graph.pb?raw=true"
+url_pbtxt = "https://github.com/ArafatTheGoldenBoy/Dynamic-Braking-System/blob/main/Carla/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt?raw=true"
 
 # Download the files to the current directory
 urllib.request.urlretrieve(url_pb, "frozen_inference_graph.pb")
